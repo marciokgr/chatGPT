21110da90d5557b3d918a6be06afee327064a7c7
Márcio Krüger <marcio.kgr@gmail.com>
2023-01-10 15:47:51 -0300

    Add project files.

---
+++ chatGPT/Program.cs
@@ -0,0 +1,21 @@
+﻿using Whetstone.ChatGPT;
+using Whetstone.ChatGPT.Models;
+
+IChatGPTClient client = new ChatGPTClient("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");
+
+Console.WriteLine("Olá, como posso te ajudar?");
+
+var msg = Console.ReadLine();
+
+var gptRequest = new ChatGPTCompletionRequest
+{
+    Model = ChatGPTCompletionModels.Davinci,
+    Prompt = msg,
+    Temperature = 1,
+    MaxTokens = 1024
+};
+
+var response = await client.CreateCompletionAsync(gptRequest);
+
+Console.WriteLine(response.GetCompletionText());
+Console.ReadLine();
