455ac95b432a2452ef41f2b976985c6814bb05c7
Márcio Krüger <marcio.kgr@gmail.com>
2023-01-10 16:03:36 -0300

    criando um dialogo!

--- chatGPT/Program.cs
+++ chatGPT/Program.cs
@@ -1,21 +1,26 @@
 ﻿using Whetstone.ChatGPT;
 using Whetstone.ChatGPT.Models;
 
-IChatGPTClient client = new ChatGPTClient("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");
+//gerar chave em https://beta.openai.com/account/api-keys
+IChatGPTClient client = new ChatGPTClient("CHAVE GERADA AQUI"); 
 
-Console.WriteLine("Olá, como posso te ajudar?");
 
-var msg = Console.ReadLine();
-
-var gptRequest = new ChatGPTCompletionRequest
+while (true)
 {
-    Model = ChatGPTCompletionModels.Davinci,
-    Prompt = msg,
-    Temperature = 1,
-    MaxTokens = 1024
-};
+    Console.WriteLine("Digite sua pergunta.");
+    Console.WriteLine();
+    var msg = Console.ReadLine();
 
-var response = await client.CreateCompletionAsync(gptRequest);
+    var gptRequest = new ChatGPTCompletionRequest
+    {
+        Model = "text-davinci-003",
+        Prompt = msg,
+        Temperature = 1,
+        MaxTokens = 1024
+    };
 
-Console.WriteLine(response.GetCompletionText());
-Console.ReadLine();
+    var response = await client.CreateCompletionAsync(gptRequest);
+
+    Console.WriteLine(response.GetCompletionText());
+    Console.WriteLine();
+}
